4ddb382a883c581e1aea0751309fe05c94eed208
Federico Nusymowicz <fedenusy@gmail.com>
2012-02-23 20:25:34 -0500

    Created Bin and BinObject classes

---
+++ src/edu/upenn/cis/cis350/algorithmvisualization/BinObject.java
@@ -0,0 +1,16 @@
+package edu.upenn.cis.cis350.algorithmvisualization;
+
+class BinObject {
+	
+	private double weight;
+	private double value;
+	
+	BinObject(double weight, double value) {
+		this.weight = weight;
+		this.value = value;
+	}
+	
+	public double getWeight() { return weight; }
+	public double getValue() { return value; }
+	
+}
