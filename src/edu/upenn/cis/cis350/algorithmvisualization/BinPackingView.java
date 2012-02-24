e570c95f5064861a7c86df518243ffef676e83b5
Federico Nusymowicz <fedenusy@gmail.com>
2012-02-24 15:34:59 -0500

    **added the file for BinPackingView

---
+++ src/edu/upenn/cis/cis350/algorithmvisualization/BinPackingView.java
@@ -0,0 +1,29 @@
+package edu.upenn.cis.cis350.algorithmvisualization;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class BinPackingView extends View {
+
+	//Constructors
+	public BinPackingView(Context c) {
+		super(c);
+	}
+	public BinPackingView(Context c, AttributeSet a) {
+		super(c, a);
+	}
+	
+	//Called when the view is displayed
+	public void onDraw(Canvas canvas) {
+		//TODO Draw initial layout
+	}
+	
+	public boolean onTouchEvent(MotionEvent event) {
+		//TODO Handle user input
+		return false;
+	}
+	
+}
