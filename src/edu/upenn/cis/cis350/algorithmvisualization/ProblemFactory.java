889c1e485067863ac0077dcda48f9879dcf081c4
Federico Nusymowicz <fedenusy@gmail.com>
2012-03-11 19:41:35 -0400

    Add getOptimalSolution method to ProblemFactory

--- src/edu/upenn/cis/cis350/algorithmvisualization/ProblemFactory.java
+++ src/edu/upenn/cis/cis350/algorithmvisualization/ProblemFactory.java
@@ -1,6 +1,5 @@
 package edu.upenn.cis.cis350.algorithmvisualization;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -9,20 +8,23 @@ import java.util.Collection;
  */
 public class ProblemFactory {
 
-	private ArrayList<BinObject> easyObjects, mediumObjects, hardObjects;
-	private ArrayList<Bin> easyBins, mediumBins, hardBins;
+	private Collection<BinObject> easyObjects, mediumObjects, hardObjects;
+	private Collection<Bin> easyBins, mediumBins, hardBins;
+	private double easyOptSol, mediumOptSol, hardOptSol;
 	
 	/**
-	 * Reads in the app config specified in res/xml/problems.xml and generates the 
-	 * corresponding Bin and BinObject collections for each difficulty.
+	 * For each difficulty: reads in the app config specified in res/xml/problems.xml,
+	 * generates the corresponding Bin and BinObject collections, and calculates
+	 * the value of the optimal solution.
 	 */
 	public ProblemFactory() {
 		//TODO
 	}
 	
 	/**
-	 * @param difficulty The difficulty of the object collection as specified in problems.xml.
-	 * @return a Collection of all BinObjects for the specified difficulty.
+	 * @param difficulty The difficulty of the problem as specified in problems.xml.
+	 * @return a Collection of all BinObjects for the specified difficulty, or null if the
+	 * difficulty is unspecified in problems.xml.
 	 */
 	public Collection<BinObject> getBinObjects(String difficulty) {
 		if ("easy".equalsIgnoreCase(difficulty)) return easyObjects;
@@ -32,14 +34,27 @@ public class ProblemFactory {
 	}
 	
 	/**
-	 * @param difficulty The difficulty of the bin collection as specified in problems.xml.
-	 * @return a Collection of all Bins for the specified difficulty.
+	 * @param difficulty The difficulty of the problem as specified in problems.xml.
+	 * @return a Collection of all Bins for the specified difficulty, or null if the
+	 * difficulty is unspecified in problems.xml.
 	 */
 	public Collection<Bin> getBins(String difficulty) {
 		if ("easy".equalsIgnoreCase(difficulty)) return easyBins;
 		else if ("medium".equalsIgnoreCase(difficulty)) return mediumBins;
 		else if ("hard".equalsIgnoreCase(difficulty)) return hardBins;
 		return null;
+	}
+	
+	/**
+	 * @param difficulty The difficulty of the problem as specified in problems.xml.
+	 * @return the value of the optimal solution for the specified difficulty, or -1 if the
+	 * difficulty is unspecified in problems.xml.
+	 */
+	public double getOptimalSolution(String difficulty) {
+		if ("easy".equalsIgnoreCase(difficulty)) return easyOptSol;
+		else if ("medium".equalsIgnoreCase(difficulty)) return mediumOptSol;
+		else if ("hard".equalsIgnoreCase(difficulty)) return hardOptSol;
+		return -1;
 	}
 	
 }
