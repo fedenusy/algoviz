26a510a4e9125703ae1dde4a3bcb0f49a0f9a2b0
Di Mu <dimu@ampersand.seas.upenn.edu>
2012-03-12 02:49:31 -0400

    ShapeObject.java

--- src/edu/upenn/cis/cis350/algorithmvisualization/ShapeObject.java
+++ src/edu/upenn/cis/cis350/algorithmvisualization/ShapeObject.java
@@ -8,6 +8,9 @@ public class ShapeObject
 	private int selColor;	
 	private int width;
 	private int length;
+	//top-left point
+	private float locx;
+	private float locy;
 	
 
 	public ShapeObject()
@@ -16,19 +19,31 @@ public class ShapeObject
 		selColor = Color.WHITE;
 		width = 50;
 		length = 50;
+		locx=300;
+		locy=300;
 	}
+	
 	
 	public ShapeObject(int basColor, int selectColor, int mywidth, int mylength)
 	{
 		baseColor = basColor;
 		selColor = selectColor;
-		/*
-		if (mywidth < 0 || mywidth > 100)
-			throw new IllegalArgumentException("Width out of bounds");
-		if (mylength < 0 || mylength > 100)
-			throw new IllegalArgumentException("Length out of bounds");*/
+	
 		width = mywidth;
 		length = mylength;
+		locx=300;
+		locy=300;
+	}
+	
+	public ShapeObject(int basColor, int selectColor, int mywidth, int mylength, int x, int y)
+	{
+		baseColor = basColor;
+		selColor = selectColor;
+
+		width = mywidth;
+		length = mylength;
+		locx=x;
+		locy=y;
 	}
 	
 	public int getBase()
@@ -50,5 +65,31 @@ public class ShapeObject
 	{
 		return length;
 	}
+
+	public float getX(){
+		return locx;
+	}
+	
+	public float getY(){
+		return locy;
+	}
+	
+	public void setX(int x){
+		locx=x;
+	}
+	
+	public void getY(int y){
+		locy=y;
+	}
+
+	public boolean collidesWith(ShapeObject s2) {
+		
+		/**
+		if( ( (x>=s2.x && x<=s2.x+s2.width) || (x+width>=s2.x && x+width<=s2.x+s2.width) )
+				&& ( (y>=s2.y && y<=s2.y+s2.width) || (y+width>=s2.y && y+width<=s2.y+s2.width) ))
+			return true;*/
+		return false;
+	}
+	
 
 }
