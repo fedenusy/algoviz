987a77a8283a0657dcdaf6cad570b6baba2c038f
Di Mu <dimu@ampersand.seas.upenn.edu>
2012-04-10 16:27:20 -0400

    start test cases

---
+++ src/edu/upenn/cis350/algoviz/TestBinObject.java
@@ -0,0 +1,14 @@
+package edu.upenn.cis350.algoviz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBinObject {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
