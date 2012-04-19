ddbaedefd9057ffc5d50bdde641823a3a6c4949d
Di Mu <dimu@minus.seas.upenn.edu>
2012-04-18 20:24:50 -0400

    menu pic changed

--- gen/edu/upenn/cis350/algoviz/R.java
+++ gen/edu/upenn/cis350/algoviz/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int button2=0x7f060002;
         public static final int button3=0x7f060004;
         public static final int button4=0x7f060003;
-        public static final int button7=0x7f06001b;
+        public static final int button7=0x7f06001c;
         public static final int chronometer1=0x7f060008;
         public static final int imageView1=0x7f06001a;
+        public static final int imageView2=0x7f06001b;
         public static final int linearLayout1=0x7f060000;
         public static final int linearLayout2=0x7f060005;
         public static final int linearLayout3=0x7f060009;
